f67d5f452f7ade5c07e473b6986c9fc7490f3ed8
aservia <60531534+aservia@users.noreply.github.com>
2020-02-20 19:33:27 -0300

    a

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,10 @@
 function mostrar()
 {
-
-var clave = prompt("ingrese el número clave.");
-
+// while(!(claveclave = prompt("ingrese el número clave.");
+while (clave != "utn750")
+{
+    clave = prompt("Clave invalida. Vuelva a ingresar la clave");
+}
+alert ("Clave correcta");
 
 }//FIN DE LA FUNCIÓN
