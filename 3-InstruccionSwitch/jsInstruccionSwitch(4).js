80df417e7d717998eed2310008b8d55aacac925f
aservia <60531534+aservia@users.noreply.github.com>
2020-02-20 19:30:25 -0300

    a

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -3,7 +3,26 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+switch (mesDelAño) 
+{
+    case "Enero":
+    case "Marzo" :
+    case "Mayo":
+    case "Julio" :
+    case "Agosto" :
+    case "Octubre" :
+    case "Diciembre":
+    alert ("Tiene 31 dias");
+    break;
+    case ("Febrero"):
+    alert("Tiene 29 dias");
+    break;
+    default :
+    alert ("Tiene 30 dias");
+
+    
+}
+
 	
 	
 
